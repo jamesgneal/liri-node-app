93546894e689570ae65deb9b465ab0ecfefb757d
Graham Neal <nealjg@gmail.com>
2018-04-09 15:48:24 -0400

    OMDB functioning and deliverable

--- liri.js
+++ liri.js
@@ -5,8 +5,9 @@ var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var request = require("request");
 
-// Capture and store node arguments
+// Capture node arguments
 var nodeArgs = process.argv;
 var whichAPI = nodeArgs[2];
 var searchParam = nodeArgs[3];
@@ -44,7 +45,7 @@ if (whichAPI === "spotify-this-song") {
     if (!searchParam) {
         // If user doesn't provide a song
         var userSong = "The Sign";
-        
+
         // Connect to spotify
         spotify
             .request('https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc')
@@ -82,6 +83,30 @@ if (whichAPI === "spotify-this-song") {
 // OMDB =================================================================================================
 if (whichAPI === "movie-this") {
     console.log(`receiving OMDB input ${searchParam}`);
+
+    if (!searchParam) {
+        // If no title is given to search
+        var queryURL = `https://www.omdbapi.com/?t="mr+nobody"&y=&plot=short&apikey=trilogy`;
+
+        // Connect to OMDB
+        request(queryURL, function (error, response, body) {
+            var movieObject = JSON.parse(body);
+            console.log(`\n ~~~~~~ Movie info for "${movieObject.Title}" ~~~~~~ \n`);
+            console.log(`Title: ${movieObject.Title}\nYear Released: ${movieObject.Year}\nIMDB Rating: ${movieObject.Ratings[0].Value}\nRotten Tomatoes Rating: ${movieObject.Ratings[1].Value}\nCountry Produced: ${movieObject.Country}\nLanguage: ${movieObject.Language}\nPlot: ${movieObject.Plot}\nActors: ${movieObject.Actors}\n`);
+        });
+
+    } else {
+        // User's title to look up
+        var title = searchParam;
+        var queryURL = `https://www.omdbapi.com/?t="${title}"&y=&plot=short&apikey=trilogy`;
+
+        // Connect to OMDB
+        request(queryURL, function (error, response, body) {
+            var movieObject = JSON.parse(body);
+            console.log(`\n ~~~~~~ Movie info for "${movieObject.Title}" ~~~~~~ \n`);
+            console.log(`Title: ${movieObject.Title}\nYear Released: ${movieObject.Year}\nIMDB Rating: ${movieObject.Ratings[0].Value}\nRotten Tomatoes Rating: ${movieObject.Ratings[1].Value}\nCountry Produced: ${movieObject.Country}\nLanguage: ${movieObject.Language}\nPlot: ${movieObject.Plot}\nActors: ${movieObject.Actors}\n`);
+        });
+    }
 }
 
 
