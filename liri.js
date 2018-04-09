e03a617009655baec8aaa03afb91caa9667a0428
Graham Neal <nealjg@gmail.com>
2018-04-09 12:43:45 -0400

    inputs working

--- liri.js
+++ liri.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
-
+var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
-
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-
 var client = new Twitter(keys.twitter);
 
 // Capture and store node arguments
@@ -13,23 +12,24 @@ var searchParam = nodeArgs[3];
 
 // TWITTER =================================================================================================
 if (whichAPI === "my-tweets") {
+    console.log(`receiving twitter input`);
 }
 
 
 
 // SPOTIFY =================================================================================================
 if (whichAPI === "spotify-this-song") {
-
+    console.log(`receiving spotify input ${searchParam}`);
 }
 
 
 // OMDB =================================================================================================
 if (whichAPI === "movie-this") {
-
+    console.log(`receiving OMDB input ${searchParam}`);
 }
 
 
 // RANDOM.TXT =================================================================================================
 if (whichAPI === "do-what-it-says") {
-    
+    console.log(`receiving do-what input`);
 }
